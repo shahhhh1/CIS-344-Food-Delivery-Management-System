fec49d9f218bd219bb9db21fef392cbdda83f93e
shahhhh1 <45056524+shahhhh1@users.noreply.github.com>
2025-11-03 01:34:28 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Contact Us</h1>
+<p>Email: johndoe@gmail.com</p>
+<br>
+<p>Phone Number: 213-124-1241</p>
+<br>
+<p>Address: 2131 Bikini Bottom </p>
+
+<a href="home.php">Return to home</a>
+</body>
+
+</html>
 
