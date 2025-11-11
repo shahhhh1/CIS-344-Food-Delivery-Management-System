f4286e39eb26046959f8a4a14ecd2a55cab0e4d9
shahhhh1 <45056524+shahhhh1@users.noreply.github.com>
2025-11-10 22:22:45 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $db_server = "localhost";
 $db_user = "root";
 $password = "Whatever your password is";
-$db_name = "rush_hour_food_delivery_service_llc";
+$db_name = "rush_hour";
 $conn ="";
 
 $conn = new mysqli($db_server, $db_user, $password, $db_name);
@@ -15,3 +15,4 @@ if($conn)
         echo "Can't connect";
     }
 ?>
+
